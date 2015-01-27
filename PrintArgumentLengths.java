b82d400627f988971943de38398eea7f68fdf1da
Josh Soriano <jsorian4@lion.lmu.edu>
2015-01-26 17:48:56 -0800

    Add usage statement print and return

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,7 +1,8 @@
 public class PrintArgumentLengths {
   public static void main(String[] args) {
     if (args.length == 0) {
-      
+        System.out.println("Usage: java PrintArugmentLengths <any number of arguments>");
+        return;
     }
   }
 }
