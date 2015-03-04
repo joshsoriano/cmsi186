1b3b6b3fd6c3b796292bcef385164ea2ee486e26
Josh Soriano <jsorian4@lion.lmu.edu>
2015-03-03 16:32:00 -0800

    Implemented Vector, x, y, add, and scale methods

--- Vector.java
+++ Vector.java
@@ -2,25 +2,34 @@ public class Vector {
 
     private double x;
     private double y;
-
+    
     public Vector(double x, double y) {
-        // Implement me!
+        this.x = x;
+        this.y = y;
     }
     
     public double x() {
-        return 42;
+        return x;
     }
     
     public double y() {
-        return 42;
+        return y;
     }
 
     public Vector add(Vector v) {
-        return null;
+        this.x = (this.x + v.x);
+        this.y = (this.y + v.y);
+        Vector addedVector = new Vector(this.x, this.y);
+        return addedVector;
+        /* Find way to add without creating a new vector */
     }
 
     public Vector scale(double magnitude) {
-        return null;
+        this.x = (this.x * magnitude);
+        this.y = (this.y + magnitude);
+        Vector scaledVector = new Vector(this.x, this.y);
+        return scaledVector;
+        /* Find way to add without creating a new vector */
     }
-}
 
+}
