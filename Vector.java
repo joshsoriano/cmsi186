12cbbabf3950516bec44cf4848c369e414bd9106
Josh Soriano <jsorian4@lion.lmu.edu>
2015-03-03 16:30:34 -0800

    Vector.java Build Start

---
+++ Vector.java
@@ -0,0 +1,26 @@
+public class Vector {
+
+    private double x;
+    private double y;
+
+    public Vector(double x, double y) {
+        // Implement me!
+    }
+    
+    public double x() {
+        return 42;
+    }
+    
+    public double y() {
+        return 42;
+    }
+
+    public Vector add(Vector v) {
+        return null;
+    }
+
+    public Vector scale(double magnitude) {
+        return null;
+    }
+}
+
