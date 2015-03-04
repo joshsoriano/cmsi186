5c7efe317554c9e0780f418fa85ee62b4dbb0482
Josh Soriano <jsorian4@lion.lmu.edu>
2015-03-03 16:12:41 -0800

    AngryBallsTestHarness Build Start

---
+++ AngryBallsTestHarness.java
@@ -0,0 +1,26 @@
+public class AngryBallsTestHarness {
+
+    private static int attempts = 0;
+    private static int successes = 0;
+
+    public static void main(String[] args) {
+        attempts = 0;
+        successes = 0;
+
+        /*test_VectorCreation();*/
+
+        System.out.println(successes + "/" + attempts + " tests passed.");
+    }
+
+    private static void displaySuccessIfTrue(boolean value) {
+        attempts++;
+        successes += value ? 1 : 0;
+
+        System.out.println(value ? "success" : "failure");
+    }
+
+    private static void displayFailure() {
+        displaySuccessIfTrue(false);
+    }
+
+}
