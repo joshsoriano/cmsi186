de4606b59ce7d38b544706d14f8d018246d889bb
Josh Soriano <jsorian4@lion.lmu.edu>
2015-03-11 01:16:18 -0700

    Removed comments

--- Vector.java
+++ Vector.java
@@ -1,8 +1,8 @@
 public class Vector {
 
-    private double x;
-    private double y;
-    
+    public double x;
+    public double y;
+
     public Vector(double x, double y) {
         this.x = x;
         this.y = y;
@@ -17,19 +17,16 @@ public class Vector {
     }
 
     public Vector add(Vector v) {
-        this.x = (this.x + v.x);
-        this.y = (this.y + v.y);
-        Vector addedVector = new Vector(this.x, this.y);
-        return addedVector;
-        /* Find way to add without creating a new vector */
+        this.x = this.x + v.x;
+        this.y = this.y + v.y;
+        Vector sum = new Vector(this.x, this.y);
+        return sum;
     }
 
     public Vector scale(double magnitude) {
-        this.x = (this.x * magnitude);
-        this.y = (this.y * magnitude);
-        Vector scaledVector = new Vector(this.x, this.y);
-        return scaledVector;
-        /* Find way to add without creating a new vector */
+        this.x = this.x * magnitude;
+        this.y = this.y * magnitude;
+        Vector vectorScaled = new Vector(this.x, this.y);
+        return vectorScaled;
     }
-
 }
