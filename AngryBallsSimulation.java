26c3b0c77f111504137447d58077e0a8167ee25c
Josh Soriano <jsorian4@lion.lmu.edu>
2015-03-16 20:46:56 -0700

    Working version of Simulation

--- AngryBallsSimulation.java
+++ AngryBallsSimulation.java
@@ -41,14 +41,44 @@ public class AngryBallsSimulation {
         Vector blueBallLocation = new Vector(input[6], input[7]);
         Vector blueBallInitialVelocity = new Vector(input[8], input[9]);
         Ball blueBall = new Ball(blueBallRadius, blueBallLocation, blueBallInitialVelocity);
-
+        
         Double grain = input[10];
         Vector acceleration = new Vector(0, -9.8);
+        double numberOfCollides = 0;
+        double timeStamp = -1;
+        double time = 0;
+        Vector redBallTimeStamp = new Vector(0, 0);
+        Vector blueBallTimeStamp = new Vector(0, 0);
 
-        while (redBall.getLocation().y() > 0) {
-            redBall.move(grain);
-            redBall.accelerate(acceleration, grain);
-            redBall.move(grain);
+        while ((redBall.getLocation().y() > 0) || (blueBall.getLocation().y() > 0)) {
+            System.out.println(redBall.getRadius() + " " + (Math.round(redBall.getLocation().x() * 10) / 10) + " " + (Math.round(redBall.getLocation().y() * 10) / 10) + " " + blueBall.getRadius() + " " + (Math.round(blueBall.getLocation().x() * 10) / 10) + " " + (Math.round(blueBall.getLocation().y() * 10) / 10));
+            if (redBall.collides(blueBall) && numberOfCollides == 0) {
+                redBallTimeStamp.x = redBall.getLocation().x();
+                redBallTimeStamp.y = redBall.getLocation().y();
+                blueBallTimeStamp.x = blueBall.getLocation().x();
+                blueBallTimeStamp.y = blueBall.getLocation().y();
+                timeStamp = time;
+                numberOfCollides += 1;
+            }
+            
+            if (redBall.getLocation().y() > 0) {
+                redBall.accelerate(acceleration, grain);
+                redBall.move(grain);
+            }
+
+            if (blueBall.getLocation().y() > 0) {
+                blueBall.accelerate(acceleration, grain);
+                blueBall.move(grain);
+            }
+            time += grain;
+        }
+
+        System.out.print("end\n");
+
+        if (timeStamp == -1) {
+            System.out.println("The balls did not collide.");
+        } else {
+            System.out.println("The balls collided at timestamp " + timeStamp + " with the red ball at (" + redBallTimeStamp.x() + ", " + redBallTimeStamp.y() + ") and the blue ball at timestamp (" + blueBallTimeStamp.x() + ", " + blueBallTimeStamp.y() + ").");
         }
     }
 }
