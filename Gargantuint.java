2b3ad797906a5e101eca2b3a424502f0d13479c9
Josh Soriano <jsorian4@lion.lmu.edu>
2015-03-23 22:49:47 -0700

    First commit, commented out, so compilable

---
+++ Gargantuint.java
@@ -0,0 +1,60 @@
+public class Gargantuint {
+/*
+    private byte[] number;
+
+    public gargantuint() {
+        return 0;
+    }
+
+    public gargantuint(String number) {
+    }
+
+    public String toString() {
+        return;
+    }
+
+
+    public Boolean equals(Gargantuint number) {
+        return false;
+
+    }
+
+    public Boolean isGreaterThan(Gargantuint number) {
+        return false;
+
+    }
+
+    public Boolean isLessThan(Gargantuint number) {
+        return false;
+
+    }
+
+    public Gargantuint minus(Gargantuint subtrahend) {
+        return;
+
+    }
+
+    public Gargantuint plus(Gargantuint addend) {
+        return;
+
+    }
+
+    public Gargantuint mod(Gargantuint divisor) {
+        return;
+
+    }
+
+    public Gargantuint times(Gargantuint factor) {
+        return;
+
+    }
+
+    public Gargantuint div(Gargantuint divisor) {
+        return;
+
+    }
+*/
+}
+
+
+
