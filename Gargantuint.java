895bfcab3cb33f1083b9142c9bb9462343b3865f
Josh Soriano <jsorian4@lion.lmu.edu>
2015-04-07 02:18:18 -0700

    Removed DropZeros, Fixed Constructors, Build of IGT/ILT

--- Gargantuint.java
+++ Gargantuint.java
@@ -7,13 +7,6 @@ public class Gargantuint {
         this ("0");
     }
 
-    public String dropZeros(String number) {
-        while (number.startsWith("0") && number.length() != 1) {
-            number = number.substring(1);
-        }
-        return number;
-    }
-
     public Gargantuint(String number) {
         number = number.trim();
 
@@ -26,11 +19,14 @@ public class Gargantuint {
             number = number.substring(1);
             isNegative = false;
         }
-        number = dropZeros(number);
+        while (number.startsWith("0") && number.length() != 1) {
+            number = number.substring(1);
+        }
 
-        digits = new byte[number.length()];
-        for (int i = 0; i < digits.length; i++) {
-            digits[digits.length - i - 1] = Byte.parseByte(number.substring(i, i + 1));
+        this.digits = new byte[number.length()];
+
+        for (int i = 0; i < this.digits.length; i++) {
+            this.digits[this.digits.length - i - 1] = Byte.parseByte(number.substring(i, i + 1));
         }
     }
 
@@ -45,34 +41,75 @@ public class Gargantuint {
         } else if (!isNegative && (digits.length != 1 || digits[0] != 0)) {
             string = '+' + string;
         }
-        return string;
-        
+        return string;        
+    }
+
+    public boolean isNegative() {
+        return this.isNegative;
     }
 
 
     public Boolean equals(Gargantuint other) {
-        return (this.toString().equals(other.toString()));
-    }
-
-    public Boolean isGreaterThan(Gargantuint other) {
-        if (!this.isNegative && other.isNegative) {
-            return true;
-        } else if (this.isNegative && !other.isNegative) {
+        if (other.isNegative() && !this.isNegative()) {
             return false;
-        } else if ((this.isNegative && other.isNegative) && (this.digits.length > other.digits.length)) {
-            return true;
-        } else if ((this.isNegative && other.isNegative) && (this.digits.length < other.digits.length)) {
+        } else if (!other.isNegative() && this.isNegative()) {
             return false;
-        } else if ((this.isNegative && other.isNegative) && (this.digits.length == other.digits.length)) {
-            return false;
+        }
+        if (this.digits.length == other.digits.length) {
+            for (int i = 0; i < this.digits.length; i++) {
+                if (this.digits[i] < other.digits[i]) {
+                    return false;
+                } else if (other.digits[i] < this.digits[i]) {
+                    return false;
+                }
+            }
         } else {
             return false;
         }
+        return true;
     }
 
-    public Boolean isLessThan(Gargantuint number) {
-        return false;
+    public Boolean isGreaterThan(Gargantuint other) {
+        if (this.digits == other.digits) {
+            return false;
+        }
 
+        if (other.isNegative() && !this.isNegative()) {
+            return false;
+        } else if (!other.isNegative() && this.isNegative()) {
+            return true;
+        } else if (other.isNegative() && this.isNegative()) {
+            if (this.digits.length > other.digits.length) {
+                return false;
+            } else {
+                return true;
+            }
+        } else if (!other.isNegative() && !this.isNegative()) {
+            if (this.digits.length > other.digits.length) {
+                return true;
+            } else if (this.digits.length < other.digits.length) {
+                return false;
+            } else {
+                for (int i = 0; i < this.digits.length; i++) {
+                    if (this.digits[i] > other.digits[i]) {
+                        return true;
+                    } else if (this.digits[i] < other.digits[i]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public Boolean isLessThan(Gargantuint other) {
+        if (this.isGreaterThan(other)) {
+            return false;
+        } else if (this.equals(other)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public Gargantuint minus(Gargantuint subtrahend) {
