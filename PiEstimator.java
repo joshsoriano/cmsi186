74184a5fe3f98518f087f7fe8dcfd5ddb3cbd41d
Josh Soriano <jsorian4@lion.lmu.edu>
2015-04-09 16:15:25 -0700

    Implemented try/catch, nfe

--- PiEstimator.java
+++ PiEstimator.java
@@ -4,25 +4,30 @@ public class PiEstimator {
         double dartsToThrow = 10000;
         double dartsThrown = 0;
         double hits = 0;
-
-        if (args.length == 1) {
-            dartsToThrow = Integer.parseInt(args[0]);
-        }
-
-        System.out.println("start");
-
-        while (dartsThrown < dartsToThrow) {
-            double x = Math.random() * 2 - 1;
-            double y = Math.random() * 2 - 1;
-            boolean hit = false;
-            if ((Math.pow(x,2) + Math.pow(y,2)) <= 1) {
-                hit = true;
-                hits++;
+        
+        try {
+            if (args.length == 1) {
+                dartsToThrow = Integer.parseInt(args[0]);
             }
-            System.out.println(x + " " + y + " " + (hit ? "in" : "out"));
-            dartsThrown++;
+
+            System.out.println("start");
+
+            while (dartsThrown < dartsToThrow) {
+                double x = Math.random() * 2 - 1;
+                double y = Math.random() * 2 - 1;
+                boolean hit = false;
+                if ((Math.pow(x,2) + Math.pow(y,2)) <= 1) {
+                    hit = true;
+                    hits++;
+                }
+                System.out.println(x + " " + y + " " + (hit ? "in" : "out"));
+                dartsThrown++;
+            }
+            System.out.println("end");
+            System.out.println("Darts: " + dartsToThrow + " Hits: " + hits +  " Estimate: " + ((hits/dartsToThrow) * 4));
+        } catch (NumberFormatException nfe) {
+            System.out.println("Supplied value is not an integer.");
+            return;  
         }
-        System.out.println("end");
-        System.out.println("Darts: " + dartsToThrow + " Hits: " + hits +  " Estimate: " + ((hits/dartsToThrow) * 4));
     }
 }
