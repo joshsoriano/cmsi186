d9419136d95866fdeaa1b39e30b5806f13e7cc36
Josh Soriano <jsorian4@lion.lmu.edu>
2015-04-09 15:47:44 -0700

    Condensed to run from main

--- PiEstimator.java
+++ PiEstimator.java
@@ -1,39 +1,28 @@
 public class PiEstimator {
 
-    public static boolean[] throwDarts (int darts) { 
-        boolean[] hits = new boolean[darts];
-        for(int i = 0; i < darts; i++) {
-        double x = Math.random() * 2;
-        double y = Math.random() * 2;
-        if((Math.pow(x,2) + Math.pow(y,2)) <= 1){
-            hits[i] = true;
-            System.out.println(x + " " + y + "  in");
-        } else {
-            hits[i] = false;
-            System.out.println(x + " " + y + "  out");
-        }
-       
-       }
-       return hits;
-    }
-
-    public static int estimate(boolean[] array, int darts) {
-        int hitsCounter = 0;
-        int percentage;
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] == true) {
-                hitsCounter++;
-            }
-        }
-        percentage = (hitsCounter / darts);
-        return (4 * percentage);
-    }
-
     public static void main(String[] args) {
-        int darts = Integer.parseInt(args[0]);
+        double dartsToThrow = 10000;
+        double dartsThrown = 0;
+        double hits = 0;
+
+        if (args.length == 1) {
+            dartsToThrow = Integer.parseInt(args[0]);
+        }
+
         System.out.println("start");
-        throwDarts(darts);
+
+        while (dartsThrown < dartsToThrow) {
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            boolean hit = false;
+            if ((Math.pow(x,2) + Math.pow(y,2)) <= 1) {
+                hit = true;
+                hits++;
+            }
+            System.out.println(x + " " + y + " " + (hit ? "in" : "out"));
+            dartsThrown++;
+        }
         System.out.println("end");
-        System.out.println("Darts: " + darts);
+        System.out.println("Darts: " + dartsToThrow + " Hits: " + hits +  " Estimate: " + ((hits/dartsToThrow) * 4));
     }
 }
