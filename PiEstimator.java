3502929ddd2abde8b9d0772dd567f27585b64882
Josh Soriano <jsorian4@lion.lmu.edu>
2015-04-09 01:24:05 -0700

    Concept adjustments and start of main

--- PiEstimator.java
+++ PiEstimator.java
@@ -1,16 +1,18 @@
 public class PiEstimator {
 
     public static boolean[] throwDarts (int darts) { 
-        boolean [] hits = new boolean[darts];
-        for(int i = 0; i < darts; i++){
+        boolean[] hits = new boolean[darts];
+        for(int i = 0; i < darts; i++) {
         double x = Math.random() * 2;
         double y = Math.random() * 2;
         if((Math.pow(x,2) + Math.pow(y,2)) <= 1){
             hits[i] = true;
-        }
-        else{
+            System.out.println(x + " " + y + "  in");
+        } else {
             hits[i] = false;
+            System.out.println(x + " " + y + "  out");
         }
+       
        }
        return hits;
     }
@@ -25,5 +27,13 @@ public class PiEstimator {
         }
         percentage = (hitsCounter / darts);
         return (4 * percentage);
+    }
+
+    public static void main(String[] args) {
+        int darts = Integer.parseInt(args[0]);
+        System.out.println("start");
+        throwDarts(darts);
+        System.out.println("end");
+        System.out.println("Darts: " + darts);
     }
 }
