3571c1afc99b87ec3086a3fdd6fea99a0cb01b0d
Josh Soriano <jsorian4@lion.lmu.edu>
2015-04-14 23:08:58 -0700

    Completed main

--- MonteCarloIntegrator.java
+++ MonteCarloIntegrator.java
@@ -1,25 +1,26 @@
 public class MonteCarloIntegrator {
-
-    private double lower;
-    private double upper;
-    private double lowerY = 0.0;
-    private double upperY = 0.0;
-    private long darts;
+    private double lowerX;
+    private double upperX;
+    private static double lowerY = 0.0;
+    private static double upperY = 0.0;
+    private static long darts;
     private Polynomial function;
     private int upperHits = 0;
     private int lowerHits = 0;
-    private double estimate;
+    private static double estimate;
+    private static double lowerBound;
+    private static double upperBound;
 
     public MonteCarloIntegrator(Polynomial function) {
         this.function = function;
     }
 
     public double integrate(double lower, double upper, long darts) {
-        this.lower = lower;
-        this.upper = upper;
+        this.lowerX = lower;
+        this.upperX = upper;
         this.darts = darts;
         for (int i = 0; i < darts; i++) {
-            double x = (upper - lower) * Math.random() + lower;
+            double x = (upperX - lowerX) * Math.random() + lowerX;
             double randomY = function.evaluatePoly(x);
             if (randomY > upperY) {
                 upperY = randomY;
@@ -28,9 +29,11 @@ public class MonteCarloIntegrator {
                 lowerY = randomY;
             }
         }
-
+        lowerY = lowerY * 1.1;
+        upperY = upperY * 1.1;
+        System.out.println("Start");
         for (int i = 0; i < darts; i++) {
-            double x = (upper - lower) * Math.random() + lower;
+            double x = (upperX - lowerX) * Math.random() + lowerX;
             double y = (upperY - lowerY) * Math.random() + lowerY;
             boolean hit = false;
             if (function.evaluatePoly(x) > 0.0 && (y > 0.0 && y < function.evaluatePoly(x))) {
@@ -42,8 +45,64 @@ public class MonteCarloIntegrator {
             }
             System.out.println(x + " " + y + " " + (hit ? "in" : "out"));
         }
-
-        estimate = (Math.abs(lowerY - upperY) * Math.abs(lower - upper)) * (((double)(upperHits-lowerHits)/((double)darts)));
+        estimate = (Math.abs(lowerY - upperY) * Math.abs(lowerX - upperX)) * (((double)(upperHits-lowerHits)/((double)darts)));
         return estimate;
+    }
+
+    public static void main(String[] args) {
+        double[] coefficients = new double[] {1.0};
+        int placeHolder;
+
+        if (args.length == 0) {
+            System.out.println("Usage: java MonteCarloIntegrator <function type> <coefficients/constants> <bound for x value> <bound for x value> <total=[# of darts] (OPTIONAL)>\n");
+            return;
+        }
+
+        if (args[0].equals("poly")) {
+            placeHolder = 1;
+            if (args[args.length - 1].startsWith("total=")) {
+                placeHolder = 0;
+                
+                try {
+                    darts = Long.parseLong(args[args.length - 1].substring(6));
+                } catch (NumberFormatException nfe) {
+                    System.out.println("One or more of the supplied values is/are not valid.");
+                    System.out.println("Usage: java MonteCarloIntegrator <function type> <coefficients/constants> <bound for x value> <bound for x value> <total=[# of darts] (OPTIONAL)>\n");
+                    return;
+                }
+            } else {
+                darts = 10000;
+            }
+
+            try {
+                lowerBound = Double.parseDouble(args[args.length - (3 - placeHolder)]);
+                upperBound = Double.parseDouble(args[args.length - (2 - placeHolder)]);
+                if (upperBound < lowerBound) {
+                    double temp = lowerBound;
+                    lowerBound = upperBound;
+                    upperBound = temp;
+                }
+                coefficients = new double[args.length - (4 - placeHolder)];
+                for (int i = 1, j = 0; i < args.length - (3 - placeHolder); i++, j++) {
+                    coefficients[j] = Double.parseDouble(args[i]);
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("One or more of the supplied values is/are not valid.");
+                System.out.println("Usage: java MonteCarloIntegrator <function type> <coefficients/constants> <bound for x value> <bound for x value> <total=[# of darts] (OPTIONAL)>\n"); 
+                return;
+            } 
+            Polynomial polynomial = new Polynomial(coefficients);
+            MonteCarloIntegrator test = new MonteCarloIntegrator(polynomial);
+            System.out.println("Minimum in range: " + lowerY);
+            System.out.println("Maximum in range: " + upperY);
+            test.integrate(lowerBound, upperBound, darts);
+            System.out.println("end");
+            System.out.println("Estimate: " + estimate);
+
+        } else if (!(args[0].equals("poly"))) {
+            System.out.println("First argument is not a supported type of function.");
+            System.out.println("Usage: java MonteCarloIntegrator <function type> <coefficients/constants> <bound for x value> <bound for x value> <total=[# of darts] (OPTIONAL)>\n");
+            return;
+        }
     }
 }
