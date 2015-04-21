932053c742dec3d7b6b77c4af32c8a63ee05f1d3
Josh Soriano <jsorian4@lion.lmu.edu>
2015-04-21 15:05:48 -0700

    Original

---
+++ MakeOptimalChangeTestHarness.java
@@ -0,0 +1,60 @@
+public class MakeOptimalChangeTestHarness {
+
+    private static int attempts = 0;
+    private static int successes = 0;
+
+    public static void main(String[] args) {
+        attempts = 0;
+        successes = 0;
+
+        test_USA();
+        // Add more!
+
+        System.out.println(successes + "/" + attempts + " tests passed.");
+    }
+
+    private static void displaySuccessIfTrue(boolean value) {
+        attempts++;
+        successes += value ? 1 : 0;
+
+        System.out.println(value ? "success" : "failure");
+    }
+
+    private static void displayFailure() {
+        displaySuccessIfTrue(false);
+    }
+
+    public static void test_USA() {
+        int[] usaDenominations = new int[] { 25, 10, 5, 1 };
+
+        Tally result = MakeOptimalChange.makeOptimalChange(usaDenominations, 99);
+        try {
+            displaySuccessIfTrue(3 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(4 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+    }
+
+}
