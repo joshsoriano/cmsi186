18681443dab0c76149dc429819ca9b2cef7b4f61
Josh Soriano <jsorian4@lion.lmu.edu>
2015-04-23 21:59:23 -0700

    Added more tests

--- Dynamic-Programming/MakeOptimalChangeTestHarness.java
+++ Dynamic-Programming/MakeOptimalChangeTestHarness.java
@@ -7,7 +7,10 @@ public class MakeOptimalChangeTestHarness {
         attempts = 0;
         successes = 0;
 
-        test_USA();
+        test_usaSMALL();
+        test_usaBIG();
+        test_randomSMALL();
+        test_randomBIG();
         // Add more!
 
         System.out.println(successes + "/" + attempts + " tests passed.");
@@ -24,19 +27,19 @@ public class MakeOptimalChangeTestHarness {
         displaySuccessIfTrue(false);
     }
 
-    public static void test_USA() {
+    public static void test_usaSMALL() {
         int[] usaDenominations = new int[] { 25, 10, 5, 1 };
 
-        Tally result = MakeOptimalChange.makeOptimalChange(usaDenominations, 99);
+        Tally result = MakeOptimalChange.makeOptimalChange(usaDenominations, 12);
         try {
-            displaySuccessIfTrue(3 == result.getElement(0));
+            displaySuccessIfTrue(0 == result.getElement(0));
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
         }
 
         try {
-            displaySuccessIfTrue(2 == result.getElement(1));
+            displaySuccessIfTrue(1 == result.getElement(1));
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
@@ -50,7 +53,106 @@ public class MakeOptimalChangeTestHarness {
         }
 
         try {
-            displaySuccessIfTrue(4 == result.getElement(3));
+            displaySuccessIfTrue(2 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+    }
+
+    public static void test_usaBIG() {
+        int[] usaDenominations = new int[] { 25, 10, 5, 1 };
+
+        Tally result = MakeOptimalChange.makeOptimalChange(usaDenominations, 256);
+        try {
+            displaySuccessIfTrue(10 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+    }
+
+    public static void test_randomSMALL() {
+        int[] randomDenominations = new int[] { 1, 7, 9, 4 };
+
+        Tally result = MakeOptimalChange.makeOptimalChange(randomDenominations, 13);
+        try {
+            displaySuccessIfTrue(0 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+    }
+
+    public static void test_randomBIG() {
+        int[] randomDenominations = new int[] { 1, 7, 9, 4 };
+
+        Tally result = MakeOptimalChange.makeOptimalChange(randomDenominations, 13);
+        try {
+            displaySuccessIfTrue(0 == result.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result.getElement(3));
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
